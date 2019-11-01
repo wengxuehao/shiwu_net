086e64ea411f25152cf1a2ff662a7e1546c6e166
xuehao.weng <42528849+Pywww@users.noreply.github.com>
2019-11-01 17:52:54 +0800

    vue访问页面信息，添加cors配置

--- apps/service/views.py
+++ apps/service/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from utils import result
+
 
 class Voice_View(View):
     def get(self, request):
@@ -13,7 +15,12 @@ class Voice_View(View):
 class Vision_View(View):
     def get(self, request):
         # print('调取视觉技术服务')
-        return render(request, 'service/Vision_technology.html')
+        # return render(request, 'service/Vision_technology.html')\
+        data = {
+            "list":["返回注册信息列表"]
+        }
+        return result.result(message="success",data=data)
+
 
 
 class NLP_View(View):
